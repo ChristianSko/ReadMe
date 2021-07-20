b251ae89463dbef44f0e6093f99f90f792989c2c
Christian Skorobogatow <christiansko@icloud.com>
2021-07-20 22:29:40 +0200

    feat: added bookModel and connect tableview to VC

---
+++ ReadMe/Book.swift
@@ -0,0 +1,14 @@
+//
+//  Book.swift
+//  ReadMe
+//
+//  Created by Christian Skorobogatow on 20/7/21.
+//
+
+import UIKit
+
+struct Book {
+    let title: String
+    let author: String
+    let image: UIImage
+}
---
+++ ReadMe/LibraryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ReadMe
+//
+//  Created by Christian Skorobogatow on 20/7/21.
+//
+
+import UIKit
+
+class LibraryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+    
+    // MARK: - Data Source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        50
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+
+}
+
