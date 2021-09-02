040c959cd07e4e315ef257e23e2395bc085790d9
Christian Skorobogatow <christiansko@icloud.com>
2021-09-02 23:07:22 +0200

    feat: added diffable datasource

--- ReadMe/LibraryViewController.swift
+++ ReadMe/LibraryViewController.swift
@@ -14,29 +14,69 @@ class LibraryHeaderView: UITableViewHeaderFooterView {
 
 
 class LibraryViewController: UITableViewController {
+    enum Section: String, CaseIterable {
+        case addNew
+        case readMe =  "Readme"
+        case finished = "Finished!"
+    }
+    
+    var dataSource: UITableViewDiffableDataSource<Section, Book>!
 
     @IBSegueAction func showDetailView(_ coder: NSCoder) -> DetailViewController? {
         
-        guard let indexPath = tableView.indexPathForSelectedRow
+        guard let indexPath = tableView.indexPathForSelectedRow,
+              let book = dataSource.itemIdentifier(for: indexPath)
         else { fatalError("Nothing Selected")}
-        
-        let book = Library.books[indexPath.row]
         
         return DetailViewController(coder: coder, book: book)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(UINib(nibName: "\(LibraryHeaderView.self)", bundle: nil), forHeaderFooterViewReuseIdentifier: LibraryHeaderView.reuseIdentifier)
+        
+        configureDataSource()
+        updateDataSource()
     }
 
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        tableView.reloadData()
+        updateDataSource()
     }
     
     
     // MARK: - Data Source
+    
+    func configureDataSource() {
+        dataSource = UITableViewDiffableDataSource(tableView: tableView) {
+            tableView, indexPath, book -> UITableViewCell? in
+            if indexPath == IndexPath(row: 0, section: 0) {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "newBookCell", for: indexPath)
+                
+                return cell
+                
+            }
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "\(BookCell.self)", for: indexPath) as? BookCell else { fatalError("could not create Bookcell")}
+            
+            
+            cell.titleLabel.text                  = book.title
+            cell.authorLabel.text                 = book.author
+            cell.bookThumbnail.image              = book.image ?? LibrarySymbol.letterSquare(letter: book.title.first).image
+            cell.bookThumbnail.layer.cornerRadius = 12
+            
+            
+            if let review = book.review {
+                cell.reviewLabel.text       = review
+                cell.reviewLabel.isHidden   = false
+            }
+            
+            cell.readMeBookMark.isHidden = !book.readMe
+            return cell
+        }
+    }
+    
+    
+    
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return section == 1 ? "Read Me!" : nil
@@ -48,7 +88,7 @@ class LibraryViewController: UITableViewController {
         guard let headerview = tableView.dequeueReusableHeaderFooterView(withIdentifier: LibraryHeaderView.reuseIdentifier) as? LibraryHeaderView
             else {return nil}
         
-        headerview.titleLabel.text = "Read me!"
+        headerview.titleLabel.text = Section.allCases[section].rawValue
         return headerview
     }
     
@@ -56,37 +96,20 @@ class LibraryViewController: UITableViewController {
         return section !=  0 ? 60 : 0
     }
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-          2
-    }
+    
     
 
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func updateDataSource() {
+        var newSnapShot = NSDiffableDataSourceSnapshot<Section, Book>()
+        newSnapShot.appendSections(Section.allCases)
+        newSnapShot.appendItems(Library.books, toSection: .readMe)
         
-        return section == 0 ? 1 : Library.books.count + 1
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        
-        if indexPath == IndexPath(row: 0, section: 0) {
-             let cell = tableView.dequeueReusableCell(withIdentifier: "newBookCell", for: indexPath)
-        
-            return cell
-            
+        let booksByReadMe: [Bool: [Book]] = Dictionary(grouping: Library.books, by: \.readMe)
+        for (readMe, books) in booksByReadMe {
+            newSnapShot.appendItems(books, toSection: readMe ? .readMe : .finished)
         }
-        
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "\(BookCell.self)", for: indexPath) as? BookCell else { fatalError("could not create Bookcell")}
-        let book = Library.books[indexPath.row]
-            
-        cell.titleLabel.text = book.title
-        cell.authorLabel.text = book.author
-        cell.bookThumbnail.image = book.image
-        cell.bookThumbnail.layer.cornerRadius = 12
-    
-        
-        return cell
+        newSnapShot.appendItems([Book.mockBook], toSection: .addNew)
+        dataSource.apply(newSnapShot, animatingDifferences: true)
     }
 
 }
